b94f919076ec296e722be7fca347de3918dd0178
MECATRONICA <mecatronica@iMac-de-MECATRONICA-70.local>
2023-06-02 17:43:58 -0600

    clase1

---
+++ api/RickandmortyApi.js
@@ -0,0 +1,25 @@
+import { View, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { API_URL } from '@env';
+import axios from 'axios';
+import RickandmortyList from '../src/componets/RickandmortyList';
+export default function RickandmortyApi() {
+	const { characters, setCharacters } = useState([]);
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const response = await axios.get(API_URL);
+				setCharacters(response.data.results);
+			} catch (error) {
+				console.log(error);
+			}
+		};
+		fetchData();
+	}, []);
+	return (
+		<View>
+			<Text>RickandmortyApi</Text>
+			<RickandmortyList characters={characters} />
+		</View>
+	);
+}
---
+++ src/componets/RickandmortyList.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+export default function RickandmortyList(props) {
+	const { characters } = props;
+	return (
+		<View>
+			<Text>RickandmortyList</Text>
+		</View>
+	);
+}
